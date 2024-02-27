28a7f0e951ab8f4afa3f0b63a5ab8ad126dcc8b3
Michael Lan <michaellan202@gmail.com>
2024-02-26 18:30:42 -0800

    feat: Add basic setup

--- lua/delimited.lua
+++ lua/delimited.lua
@@ -2,6 +2,12 @@ local api = vim.api
 local severity = vim.diagnostic.severity
 local M = {}
 
+M.settings = {}
+
+function M.setup(tbl)
+  M.settings = tbl
+end
+
 local function hlgroup(d)
 	if d.severity == severity.ERROR then
 		return "EphemeralError"
@@ -14,7 +20,7 @@ local function hlgroup(d)
 	end
 end
 
-local function diagnostic_hl(d)
+local function diagnostic_hl(d, dopts)
 	vim.g.edh_tracker = vim.g.edh_tracker or 0
 
 	local bufnr = api.nvim_get_current_buf()
@@ -25,13 +31,16 @@ local function diagnostic_hl(d)
 	vim.g.edh_tracker = (vim.g.edh_tracker + 1) % 500
 
 	api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-	vim.cmd([[IBLDisable]])
+	if dopts.pre then
+		dopts.pre()
+	end
 	vim.highlight.range(bufnr, ns, hlgroup(d), { d.lnum, d.col }, { d.end_lnum, d.end_col })
 
 	return old_edh_tracker
 end
 
-local function diagnostic_hl_set_trigger(bufnr, old_tracker, old_cursor)
+local function diagnostic_hl_set_trigger(bufnr, old_tracker, dopts)
+	local old_cursor = api.nvim_win_get_cursor(0)
 	local ns = api.nvim_create_namespace("ephemeraldiagnostichighlight")
 	api.nvim_create_autocmd({ "CursorMoved" }, {
 		callback = function()
@@ -41,37 +50,39 @@ local function diagnostic_hl_set_trigger(bufnr, old_tracker, old_cursor)
 			end
 			if vim.g.edh_tracker == (old_tracker + 1) % 500 then
 				api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-				vim.cmd([[IBLEnable]])
+				if dopts.post then
+					dopts.post()
+				end
 			end
 			return true
 		end,
 	})
 end
 
-function M.goto_next(opts)
+function M.goto_next(opts, dopts)
+	dopts = dopts or {}
 	local bufnr = api.nvim_get_current_buf()
 	local d = vim.diagnostic.get_next(opts)
 	if not d then
 		vim.diagnostic.goto_next(opts)
 		return
 	end
-	local old_tracker = diagnostic_hl(d)
+	local old_tracker = diagnostic_hl(d, dopts)
 	vim.diagnostic.goto_next(opts)
-	local old_cursor = api.nvim_win_get_cursor(0)
-	diagnostic_hl_set_trigger(bufnr, old_tracker, old_cursor)
+	diagnostic_hl_set_trigger(bufnr, old_tracker, dopts)
 end
 
-function M.goto_prev(opts)
+function M.goto_prev(opts, dopts)
+	dopts = dopts or {}
 	local bufnr = api.nvim_get_current_buf()
 	local d = vim.diagnostic.get_prev(opts)
 	if not d then
 		vim.diagnostic.goto_prev(opts)
 		return
 	end
-	local old_tracker = diagnostic_hl(d)
+	local old_tracker = diagnostic_hl(d, dopts)
 	vim.diagnostic.goto_prev(opts)
-	local old_cursor = api.nvim_win_get_cursor(0)
-	diagnostic_hl_set_trigger(bufnr, old_tracker, old_cursor)
+	diagnostic_hl_set_trigger(bufnr, old_tracker, dopts)
 end
 
 return M
